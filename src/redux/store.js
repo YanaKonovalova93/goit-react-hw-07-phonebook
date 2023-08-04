62876b0e88572cfbb77671393e110df086f76744
YanaKonovalova93 <mikobacteria@gmail.com>
2023-08-04 11:10:11 +0300

    +

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,38 +1,35 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import {
-  persistStore,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
 import { contactsSlice } from './contactsSlice';
-import { filterReducer } from './filterSlice';
+import { filterSlice } from './filterSlice';
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
-export const persistedReducerContact = persistReducer(
-  persistConfig,
-  contactsSlice.reducer
-);
+// const persistConfig = {
+//   key: 'contacts',
+//   storage,
+// };
+// export const persistedReducerContact = persistReducer(
+//   persistConfig,
+//   contactsSlice.reducer
+// );
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: persistedReducerContact,
+//     filter: filterReducer,
+//   },
+//   middleware: getDefaultMiddleware =>
+//     getDefaultMiddleware({
+//       serializableCheck: {
+//         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+//       },
+//     }),
+// });
+
+// export const persistor = persistStore(store);
+
 
 export const store = configureStore({
   reducer: {
-    contacts: persistedReducerContact,
-    filter: filterReducer,
+    contact: contactsSlice,
+    filter: filterSlice,
   },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-
-export const persistor = persistStore(store);
